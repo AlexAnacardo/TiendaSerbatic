a45c6e718b6da6a899b8e9e1a9eff3e6f51cc972
Alex Asensio <125253704+AlexAnacardo@users.noreply.github.com>
2025-04-07 00:12:14 +0200

    Delete TiendaSerbatic directory

--- TiendaSerbatic/src/main/java/com/example/demo/repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.CompraVO;
-
-public interface CompraRepository extends JpaRepository<CompraVO, CompraVO.CompraId>{
-	int countByIdCliente(Long idCliente);
-}
--- TiendaSerbatic/src/main/java/com/example/demo/service/CompraService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.CompraVO;
-
-public interface CompraService {
-	public void insertar(CompraVO compra);
-	
-	public int getByUser(Long idCliente);
-}
